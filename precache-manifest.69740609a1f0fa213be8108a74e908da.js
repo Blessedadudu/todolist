f486466868a6a4f5a1567d4b1ecb35ab9bce79ed
Blessedadudu <47356974+Blessedadudu@users.noreply.github.com>
2019-10-17 11:52:11 +0100

    Updates

---
+++ precache-manifest.69740609a1f0fa213be8108a74e908da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a139a7b80a3d052e94dfd361b3d835f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a00b085cb7396eaae2d1",
+    "url": "/todolist/static/css/main.c34398e9.chunk.css"
+  },
+  {
+    "revision": "2c01971455d2c21782ef",
+    "url": "/todolist/static/js/2.0ae43b99.chunk.js"
+  },
+  {
+    "revision": "a00b085cb7396eaae2d1",
+    "url": "/todolist/static/js/main.0ba1f420.chunk.js"
+  },
+  {
+    "revision": "ece26b232c0edbca84d9",
+    "url": "/todolist/static/js/runtime-main.766c1e21.js"
+  }
+]);
